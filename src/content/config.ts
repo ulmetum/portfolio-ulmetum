c29aebc25ede77d51d446150b3eb6d99e48edd2a
ulmetum <ulmetum@gmail.com>
2025-04-07 16:31:23 +0200

    Creacion del config.ts para el manejo de los datos

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { file } from 'astro/loaders'
+import { defineCollection, z } from 'astro:content'
+
+const proyects = defineCollection({
+  loader: file('src/data/proyects.json'),
+  schema: z.object({
+    id: z.number(),
+    featuredImage: z.string(),
+    title: z.string(),
+    description: z.string(),
+    link: z.string(),
+    tags: z.array(z.string()),
+  }),
+})
+
+export const collections = { proyects }
